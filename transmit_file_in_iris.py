e8cfaf28f5447d6118e65a0b01841d728655a06f
于吉军 <yujijun@yujijun.dfci.harvard.edu>
2019-10-03 15:10:33 -0400

    update for transmit all 906 files in iris

--- transmit_file_in_iris.py
+++ transmit_file_in_iris.py
@@ -13,10 +13,10 @@ import pyBigWig
 import pandas as pd 
 from shutil import copyfile
 import os
-
+import glob
 basic_path = '/project/dev/jjyu/DNase_summary'
 Input_path = '%s/Input' %basic_path
-filter_data = pd.read_csv('%s/04-Homo_sapiens_ca_DNase_QC(906)_non-info-tissue_FRiP0p25(543).csv' %Input_path,sep=",")
+filter_data = pd.read_csv('%s/02-Homo_sapiens_ca_DNase_QC_906.txt' %Input_path,sep=",")
 
 #filter_data = pd.read_csv('/Users/yujijun/Documents/01-Work/02-HiglassProject/01-DataFilter/Output_data/04-Homo_sapiens_ca_DNase_QC(906)_non-info-tissue_FRiP0p25(543).csv', sep=',')
 #if True:
@@ -38,31 +38,57 @@ filter_data = pd.read_csv('%s/04-Homo_sapiens_ca_DNase_QC(906)_non-info-tissue_F
 #            bw.close()
 #            print("\t")
 #else:
-#    #transmit the file correct and with any of chr information
-#    for i in range(len(filter_data)):
-#        Output_path = '%s/DNase_bw_v1/' %basic_path
-#        print("This is %s .th file." %(i+1) )
-#        #input_folder = 'dataset' + str(filter_data.DatsetId[i])
-#        bigwig_path = filter_data.bigwig[i]
-#        print(bigwig_path)
-#        try:
-#            bw = pyBigWig.open(bigwig_path)
-#        except RuntimeError:
-#            print("This is a error file.")
-#            continue
-#        filepath,filename = os.path.split(bigwig_path)
-#        Output_path = Output_path + filename
-#        copyfile(bigwig_path,Output_path)  
-#        bw.close()
-#        print("\t")
-
-
-       
+    #transmit the file correct and with correct file and any of chr information
+bw_error_list = [] 
 for i in range(len(filter_data)):
-    submit_path= '%s/summit_file/' %basic_path
-    print("This is %s .th file." %(i+1))
-    summit_path_ori = filter_data.summit[i]
-    filepath,filename = os.path.split(summit_path_ori)
-    submit_path = submit_path + filename
-    copyfile(summit_path_ori,submit_path)
+    Output_path = '%s/DNase_bw_v1/' %basic_path
+    print("This is %s .th file." %(i+1) )
+    #input_folder = 'dataset' + str(filter_data.DatsetId[i])
+    bigwig_path = filter_data.bigwig[i]
+    filepath,filename = os.path.split(bigwig_path)
+    all_ex_filelist = glob.glob('%s/DNase_bw_v1/*.bw' %basic_path)
+    all_ex_filenamelist = [os.path.split(i)[1] for i in all_ex_filelist]
+    if filename in all_ex_filenamelist:
+        print("You already have this file.")
+        continue
+    try:
+        bw = pyBigWig.open(bigwig_path)
+    except RuntimeError:
+        print("This is a error file.")
+        continue
+    Output_path = Output_path + filename
+    copyfile(bigwig_path,Output_path)  
+    bw.close()
     print("\t")
+
+with open("%s/bw_error.txt" %basic_path) as f:
+    for item in bw_error_list:
+        f.write("%s\n" % item)
+        
+#bw_error_list = []   
+#for i in range(len(filter_data)):
+#    bigwig_path= '%s/DNase_bw_v1/' %basic_path
+#    print("This is %s .th file." %(i+1))
+#    summit_path_ori = filter_data.bigwig[i]
+#    filepath,filename = os.path.split(summit_path_ori)
+#    
+#    all_ex_filelist = glob.glob('%s/DNase_bw_v1/*.bw' %basic_path)
+#    all_ex_filenamelist = [os.path.split(i)[1] for i in all_ex_filelist]
+#    if filename in all_ex_filenamelist:
+#        print("You already have this file.")
+#        continue
+#    try:
+#        bw = pyBigWig.open(summit_path_ori)
+#    except RuntimeError:
+#        print("This is a error file.")
+#        bw_error_list.append(filename)
+#        continue
+#    submit_path = submit_path + filename
+#    copyfile(summit_path_ori,submit_path)
+#    bw.close()
+#    print("\t")
+#
+#with open("%s/bw_error.txt" %basic_path) as f:
+#    for item in bw_error_list:
+#        f.write("%s\n" % item)
+        
